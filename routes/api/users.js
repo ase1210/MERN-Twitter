009279be85d67e26813bef4e6e330414bf20fe88
DrewE <andrew.engelstein@gmail.com>
2019-03-09 19:03:55 -0800

    Add jwt to user login

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,8 @@ const express = require("express");
 const router = express.Router();
 const bcrypt = require("bcryptjs");
 const User = require("../../models/User");
+const jwt = require("jsonwebtoken");
+const keys = require('../../config/keys');
 
 router.get("/test", (req, res) =>
   res.json({
@@ -42,16 +44,38 @@ router.post("/login", (req, res) => {
   const email = req.body.email;
   const password = req.body.password;
 
-  User.findOne({ email }).then(user => {
+  User.findOne({
+    email
+  }).then(user => {
     if (!user) {
-      return res.status(404).json({ email: "This user does not exist" });
+      return res.status(404).json({
+        email: "This user does not exist"
+      });
     }
 
     bcrypt.compare(password, user.password).then(isMatch => {
       if (isMatch) {
-        res.json({ msg: "Success" });
+        const payload = {
+          id: user.id,
+          handle: user.handle,
+          email: user.email
+        };
+
+        jwt.sign(
+          payload,
+          keys.secretOrKey, {
+            expiresIn: 3600
+          },
+          (err, token) => {
+            res.json({
+              success: true,
+              token: 'Bearer ' + token
+            });
+          });
       } else {
-        return res.status(400).json({ password: "Incorrect password" });
+        return res.status(400).json({
+          password: "Incorrect password"
+        });
       }
     });
   });
