ba8b127f6a45c7fea2ef98d8291f66e250704253
DrewE <andrew.engelstein@gmail.com>
2019-03-10 11:54:08 -0700

    Add validations for Tweet input

---
+++ validation/tweets.js
@@ -0,0 +1,25 @@
+const validText = require('./valid-text');
+const Validator = require('validator');
+
+module.exports = function validateTweetInput(data) {
+  let errors = {};
+
+  data.text = validText(data.text) ? data.text : "";
+
+  if (!Validator.isLength(data.text, {
+      min: 5,
+      max: 140
+    })) {
+    errors.text = "Text must be between 5 and 140 characters";
+  }
+
+  if (Validator.isEmpty(data.text)) {
+    errors.text = "Text field is required";
+  }
+
+  return {
+    errros,
+    isValid: Object.keys(errors).length === 0
+  };
+
+};
